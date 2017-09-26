756dd2b53233168cd6bd386e2a35b3c5f26cd5c2
Douglas Alves <douglas.expalves@gmail.com>
2017-09-26 12:27:37 -0300

    atualizações

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,49 +1,64 @@
-import { Component } from '@angular/core'
-import { NavController, LoadingController } from 'ionic-angular'
+import { FormControl } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { ActionSheetController, Content, LoadingController, NavController } from 'ionic-angular';
 import { InAppBrowser } from 'ionic-native';
 
+import { RedditServiceProvider } from '../../providers/reddit-service/reddit-service';
+
+
 import { Http } from '@angular/http'
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
-  public feeds : Array<object>
+  @ViewChild(Content) content:Content;
+  public feeds : Array<any>
   private url : string = 'https://www.reddit.com/new.json'
-  public olderPosts = 'https://www.reddit.com/new.json?after='
+  private olderPosts = 'https://www.reddit.com/new.json?after='
+  private newerPosts: string = "https://www.reddit.com/new.json?before="
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
+  public searchTerm : any;
+  public searchTermControl : FormControl;
 
   constructor(
     public navCtrl: NavController,
     public http: Http,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    public actionSheetCtrl : ActionSheetController,
+    public redditService: RedditServiceProvider
   ) {
+
     this.fetchContent()
+    this.searchTermControl = new FormControl();
+    this.searchTermControl.valueChanges.debounceTime(1000).distinctUntilChanged().subscribe(search => {
+      if (search !== '' && search) {
+        this.filterItems();
+      }
+    }) 
+
   }
 
   fetchContent(): void {
     let loading = this.loadingCtrl.create({
-      content: 'Carregando Novidades'
+      content: 'Carregando Novidades...'
     })
 
     loading.present()
     
-    this.http
-      .get(this.url)
-      .map(res => res.json())
-      .subscribe(data => {
-        this.feeds = data.data.children
-        
-        this.feeds.forEach((e : any)=>{
-          if((!e.data.thumbnail) || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ){
-            e.data.thumbnail = 'https://www.kelleysislandchamber.com/wp-content/uploads/2014/11/directory-sample.png';
-          }
-        })
+    this.redditService.fetchData(this.url).then(data => {
+      this.feeds = data;
+      this.noFilter = this.feeds;
+      loading.dismiss();
+    })
 
-        // Exibindo conteúdo do array no console do browser
-        loading.dismiss()
-      })
   }
 
   itemSelected(url) : void {
@@ -52,17 +67,90 @@ export class HomePage {
 
   doInfinite(scroll) : void {
     let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1]['data'].name : "";
+    if (paramsUrl){
+      this.redditService.fetchData(this.olderPosts + paramsUrl).then(data => {
+      this.feeds = data;
+      this.noFilter = this.feeds;
+      scroll.complete();
+      })
+    }
+  }  
     
-          this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
-            .subscribe(data => {
-            
-              this.feeds = this.feeds.concat(data.data.children);
-              
-              this.feeds.forEach((e, i, a) => {
-                if (!e['data'].thumbnail || e['data'].thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {  
-                  e['data'].thumbnail = 'http://www.redditstatic.com/icon.png';
-                }
-              })
-              scroll.complete();
-            }); 
-      }  }
+    doRefresh(scroll) : void {
+      let paramsUrl = this.feeds[0].data.name;
+      
+      if (paramsUrl){
+        this.redditService.fetchData(this.newerPosts + paramsUrl).then(data => {
+          this.feeds = data;
+          this.noFilter = this.feeds;
+          scroll.complete();
+        })
+      }
+      
+    }
+
+    
+    
+    filterItems() : void {
+      this.hasFilter = false;
+      this.feeds = this.noFilter.filter((item) => {
+        return item.data.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+      });
+    }
+    
+    
+    showFilters() : void {
+      this.content.scrollToTop();
+      let actionSheet = this.actionSheetCtrl.create({
+        title : 'Opções de Filtro',
+        buttons : [{
+          text: 'Music',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "music");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Movies',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "movies");
+            this.hasFilter = true;
+          }
+        },{
+          text: 'Games',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "gaming");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Pictures',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "pics");
+            this.hasFilter = true;
+          }
+        },                
+        {
+          text: 'Ask Reddit',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "askreddit");
+            this.hasFilter = true;
+          }
+        }
+        ,{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.feeds = this.noFilter;
+            this.hasFilter = false;
+          }
+        }
+        ]
+      });
+  
+      actionSheet.present();
+  
+    } 
+  
+    
+  }
